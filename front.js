1d13ba9174d02c23bff2b10409124dd2cd71da5e
Gwinbleind <borislav31@ya.ru>
2019-11-08 14:23:19 +0300

    Login/Register
    Рабочий вариант с логином и регистрацией пользователя. Проверка ввода данных регулярками. Работает пока только на user.html

--- front.js
+++ front.js
@@ -156,6 +156,9 @@ Vue.component('cart-product-table-component', {
    },
 });
 
+const regMailExp = new RegExp('(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])',); //E-mail official standard
+const regLoginExp = new RegExp('^[a-z][a-z0-9-_\\.]{1,19}$','i'); //Login 2-20 chars latin+nums
+const regPasswordExp = new RegExp('(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$'); //Pswd 8+ characters latin+nums+symbols
 const app = new Vue({
    el: '#root',
    data: {
@@ -167,15 +170,16 @@ const app = new Vue({
          totalCost: 0,
          items: [],
       },
-      userName: '',
-      userPassword: '',
+      // userName: '',
+      // userPassword: '',
+      logOrRegStatus: 0,
       loginForm: {
          Name: '',
          Password: '',
       },
       registerForm: {
-         Name: '',
-         Password: '',
+         Age: '',
+         Mail: '',
       },
       user: {
          name: '',
@@ -186,7 +190,11 @@ const app = new Vue({
    methods: {
       fetchRequest(url) {
          return fetch(url)
-            .then(response => response.json(), reason => reason)
+            .then(response => {
+               if (response.ok) {
+                  return response.json()
+               } else {throw new Error(`${response.status}`)}
+            })
       },
       fetchCatalog() {
          this.fetchRequest('/catalog')
@@ -195,32 +203,29 @@ const app = new Vue({
                this.filteredCatalog = items;
             })
       },
-      fetchCart() {
-         this.fetchRequest('/cart').then(items => {
-            this.cart.items = items
+      fetchCart(url) {
+         return this.fetchRequest(url).then(items => {
+            this.cart.items = items;
+            return items
          });
       },
-      fetchCurrentUser() {
+      fetchCurrentUsername() {
          return this.fetchRequest('/userSettings')
             .then(settings => {
-               return new Promise(((resolve, reject) => {
                   if (settings.username !== '') {
                      this.user.name = settings.username;
-                     resolve(settings.username);
-                  } else {reject('empty name')}
-               }))
-            });
-      },
+                  } else {throw new Error('empty name')}
+               })
+            },
       fetchUser() {
          return this.fetchRequest(`/users/${this.user.name}`)
             .then(user => {
                if (user.id) {
-                  // OK
                   this.user.name = user.id;
                   this.user.age = user.age;
                   this.user.mail = user["e-mail"];
                } else {
-                  // Err
+                  throw new Error('id is undefined')
                }
                // return user
             })
@@ -240,6 +245,116 @@ const app = new Vue({
       cartAmountChangeHandler(item) {
          return this.updateCart(item.id, item.amount)
       },
+      logoutClickHandler() {
+         //Удаление записи с сервера
+         fetch(`/userSettings`, {
+            method: 'PATCH',
+            body: JSON.stringify({username: ''}),
+            headers: {
+               'Content-type': 'application/json',
+            }
+         }).then(response => response.json());
+         //Кинуть корзину в юзера
+         fetch(`/users/${this.user.name}`, {
+            method: 'PATCH',
+            body: JSON.stringify({cart: this.cart.items}),
+            headers: {
+               'Content-type': 'application/json',
+            }
+         }).then(response => response.json());
+         //удаляем корзину
+         this.fetchCart('/cart').then(items => {
+            items.forEach(item => {
+               this.delete(item.id)
+            })
+         }).then(() => {
+            this.cart.items = [];
+         });
+         //Удаляем самого юзера
+         this.user = {name:'',age:'',};
+      },
+      loginClickHandler() {
+         if (this.loginForm.Name !== '') {
+            if (!this.logOrRegStatus) {  //Login
+               fetch(`/users/${this.loginForm.Name}`)
+               // Проверка логина
+                  .then(response => {
+                     if (response.ok) {
+                        return response.json()
+                     } else {
+                        throw new Error('User not found')
+                     }
+                  })
+                  // Проверка пароля
+                  .then(user => {
+                     if (this.loginForm.Password === user.password) {
+                        return user
+                     } else {
+                        throw new Error('Password is incorrect')
+                     }
+                  })
+                  //Записываем, что юзер успешно вошел
+                  .then(user => {
+                     this.user.name = user.id;
+                     this.user.age = user.age;
+                     this.user.mail = user["e-mail"];
+                     fetch(`/userSettings`, {
+                        method: 'PATCH',
+                        body: JSON.stringify({username: user.id}),
+                        headers: {
+                           'Content-type': 'application/json',
+                        }
+                     });
+                     return user
+                  })
+                  // Забрать корзину юзера
+                  .then(user => {
+                     if (user.cart.length+1) {
+                        this.cart.items = user.cart;
+                        user.cart.forEach(item => {
+                           this.add('/cart',item)
+                        })
+                     } else {this.cart.items = []}
+                  })
+                  .catch(error => alert(error))
+            } else {  //Register
+               fetch(`/users/${this.loginForm.Name}`)
+               // Проверка логина
+                  .then(response => {
+                     if (!response.ok) {
+                        return !response.ok
+                     } else {
+                        throw new Error('User already exist')
+                     }
+                  })
+                  //Проверка введенных данных
+                  .then(() => {
+                     if (!regLoginExp.test(this.loginForm.Name)) {throw new Error('Login must be 2-20 symbols from latin and numbers')}
+                     if (!regPasswordExp.test(this.loginForm.Password)) {throw new Error('Password must be at least 8 characters, including latin and special symbols and numbers')}
+                     if (!regMailExp.test(this.registerForm.Mail)) {throw new Error('Enter valid e-mail')}
+                  })
+                  //Создание учетки
+                  .then(() => {
+                     let user = {
+                        id: this.loginForm.Name,
+                        password: this.loginForm.Password,
+                        age: this.registerForm.Age,
+                        'e-mail': this.registerForm.Mail,
+                     };
+                     this.loginForm.Name = '';
+                     this.loginForm.Password = '';
+                     this.registerForm.Age = '';
+                     this.registerForm.Mail = '';
+                     this.add(`/users`, user)
+                  })
+                  .catch(error => alert(error))
+            }}},
+      loginOrRegisterClickHandler(type) {
+         this.logOrRegStatus = type
+      },
+      loginButtonClass(type) {
+         return type ? `my_account__button_active button div_flex` : `my_account__button_inactive button div_flex`
+      },
       searchById(array,id) {
          let index = array.map(item => +item.id).indexOf(+id);
          return index
@@ -253,7 +368,7 @@ const app = new Vue({
                })
          } else {
             let item = this.catalog[this.searchById(this.catalog,id)];
-            return this.add(item).then(item => {
+            return this.add('/cart',item).then(item => {
                this.cart.items.push(item);
             });
          }
@@ -289,8 +404,8 @@ const app = new Vue({
             }
          }).then(response => response.json())
       },
-      add(item) {
-         return fetch('/cart', {
+      add(url,item) {
+         return fetch(url, {
             method: 'POST',
             body: JSON.stringify(item),
             headers: {
@@ -320,20 +435,20 @@ const app = new Vue({
       },
       showCatalog: function () {
          return this.filteredCatalog.length > 0
+      },
+      logOrReg: function () {
+         return this.logOrRegStatus ? 'Register' : 'Login'
       }
    },
    mounted: function () {
       this.fetchCatalog();
-      this.fetchCurrentUser()
+      this.fetchCurrentUsername()
          .then(() => this.fetchUser())
          .then(() => {
-            this.fetchRequest(`/users/${this.user.name}`)
-               .then(user => {
-                  this.cart.items = user.cart
-               })
+            this.fetchCart('/cart');
          })
          .catch(error => {
-            this.fetchCart();
+            this.fetchCart('/cart');
          });
    },
 });
